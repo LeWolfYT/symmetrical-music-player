e04f5db0dc4a41e05b70e4a018c70ad911c46243
LeWolfYT <pjhartiii@icloud.com>
2023-12-23 16:51:38 -0500

    wow new update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 base = "Win32GUI" if sys.platform == "win32" else None
 
 build_exe = {
-    "packages": ["pygame"],
+    "packages": ["pygame", "tinytag", "eyed3"],
     'excludes':['PyQt4', 'PyQt5', 'PySide', 'PySide2', 'IPython', 'jupyter_client', 'jupyter_core', 'ipykernel', 'ipython_genutils', 'pytz', 'asyncio', 'pandas', 'psutil', 'numpy', 'pygments', 'scipy', 'openpyxl', 'matplotlib', 'docutils', 'sqlalchemy', 'sqlite3', 'tornado', 'libssl', 'pyportmidi', 'pycrypto', 'crypto', 'http', 'email', 'xml', 'xmlrpc', 'macholib'],
 }
 
--- tkmusic.py
+++ tkmusic.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
+from tinytag import TinyTag as tnt
+import eyed3 as e3
+from PIL import Image, ImageTk
 import pygame
-import time
+import io
 import os
 
 class MusicPlayer:
@@ -9,7 +12,7 @@ class MusicPlayer:
         self.root = root
         self.root.title("Simple Music Player")
         self.root.geometry("400x155")
-        self.root.resizable(False, False)
+        self.root.resizable(True, False)
 
         self.playing = False
         self.loop = False
@@ -20,11 +23,8 @@ class MusicPlayer:
         self.select_button = tk.Button(self.root, text="Select Song", command=self.select_song)
         self.select_button.pack(anchor="w", padx=10)
 
-        self.play_button = tk.Button(self.root, text="Play", command=self.play_music)
-        self.play_button.pack(anchor="w", padx=10)
-
-        self.stop_button = tk.Button(self.root, text="Stop", command=self.stop_music)
-        self.stop_button.pack(anchor="w", padx=10)
+        self.play_pause = tk.Button(self.root, text="⏵", command=self.play_music)
+        self.play_pause.pack(anchor="w", padx=10)
         
         self.loopcheck = tk.Checkbutton(self.root, text="Loop", command=self.toggleloop)
         self.loopcheck.pack(anchor="w", padx=10)
@@ -37,9 +37,35 @@ class MusicPlayer:
         self.volume_slider.set(100)
 
     def select_song(self):
-        song_path = filedialog.askopenfilename(filetypes=[("Audio Files", ".mp3 .wav .ogg .xm .mod")])
+        song_path = filedialog.askopenfilename(filetypes=[("Audio Files", ".mp3 .wav .ogg .xm .mod .it .s3m .flac")])
+        if song_path in [None, ""]:
+            return
+        try:
+            sng = tnt.get(song_path)
+            if sng.album != None:
+                song_name = f"{sng.album} - {sng.title}"
+            else:
+                song_name = sng.title
+        except:
+            song_name = os.path.basename(song_path)
+        try:
+            sngi = e3.load(song_path)
+            print(sngi.tag.images[0].mime_type[6:])
+            img = Image.open(io.BytesIO(sngi.tag.images[0].image_data)).resize((128, 128), Image.BOX)
+            icon = ImageTk.PhotoImage(image=img, width=128, height=128)
+            self.iconc = tk.Label(self.root, width=128, height=128, image=icon)
+            self.iconc.image = icon
+            self.iconc.place_forget()
+            self.iconc.place(anchor="e", relx=1, rely=0.5, x=-56, width=128, height=128)
+            self.iconc.lower(self.song_label)
+        except:
+            self.iconc = tk.Label(self.root, width=128, height=128)
+            self.iconc.place_forget()
+            self.iconc.place(anchor="e", relx=1, rely=0.5, x=-56, width=128, height=128)
+        if song_name == None:
+            song_name = os.path.basename(song_path)
         if song_path:
-            self.song_label.config(text="Loaded: " + os.path.basename(song_path))
+            self.song_label.config(text=song_name)
             self.song_path = song_path
             self.song_length = pygame.mixer.Sound(song_path).get_length()
             pygame.mixer.music.load(self.song_path)
@@ -52,6 +78,11 @@ class MusicPlayer:
                 else:
                     pygame.mixer.music.play(0)
                 self.playing = True
+                self.play_pause.configure(text="⏹")
+            else:
+                pygame.mixer.music.stop()
+                self.playing = False
+                self.play_pause.configure(text="⏵")
 
     def stop_music(self):
         if self.playing:
